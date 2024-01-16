284ec11a6121146d7c3787ffef6d64d32f209979
Lucas Neno <lucasneno@gmail.com>
2024-01-16 13:00:00 +0000

    fix: rename project

--- secretsanta/urls.py
+++ secretsanta/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for sceretsanta project.
+URL configuration for secretsanta project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
