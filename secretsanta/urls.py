433031165168bbacd80b9f853afdf670da5a5e2a
Lucas Neno <lucasneno@gmail.com>
2024-01-17 08:46:14 +0000

    chore: installed and configured Simple JWT

--- secretsanta/urls.py
+++ secretsanta/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api-auth/', include('rest_framework.urls')), #browsable API
+    path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'), #Simple JWT
+    path('api/token/refresh', TokenRefreshView.as_view(), name='token_refresh'),#Simple JWT
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'), # Simple JWT, allow API users to verify HMAC-signed tokens without having access to your signing key
 ]
